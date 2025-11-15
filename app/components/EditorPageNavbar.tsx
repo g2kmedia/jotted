fee45643ef26299590b6f3ab0df8f36399b8ac54
g2kmedia <>
2025-11-15 23:06:20 +0100

    fixed back button to always go one level higher in the path

--- app/components/EditorPageNavbar.tsx
+++ app/components/EditorPageNavbar.tsx
@@ -1,11 +1,27 @@
+"use client"
+
 import Link from "next/link";
 import MeatballMenu from "./MeatballMenu";
-import { ArrowLeft } from 'lucide-react';
+import { ArrowLeft } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export default function EditorPageNavbar() {
+    const pathname = usePathname();
+
+    const getParentPath = (path: string): string => {
+        const segments = pathname.split("/");
+        
+        if (segments.length <= 2) return "/";
+
+        segments.pop();
+        return segments.join("/") || "/";
+    }
+
+    const parentPath = getParentPath(pathname);
+
     return (
         <nav className="m-2 p-2 h-14 flex justify-between items-center border-b-1 border-foreground">
-            <Link href={"/notes"} className="flex">
+            <Link href={parentPath} className="flex">
                 <button className="flex hover:cursor-pointer items-center">
                     <ArrowLeft />
                 </button>
