9190c1ab9cc0f9e76ddb57be4d08643fbe182e25
g2kmedia <>
2026-04-05 22:15:38 +0200

    improved serwist offline page

--- app/~offline/page.tsx
+++ app/~offline/page.tsx
@@ -1,8 +1,8 @@
 export default function OfflinePage() {
   return (
-    <div>
-      <h1>You're offline</h1>
-      <p>Check your connection and try again.</p>
+    <div className="h-1/3 flex flex-col justify-center items-center">
+      <h1 className="text-xl m-4">You're offline and have stumbled upon a page that is currently not available without an internet connection.</h1>
+      <h1 className="text-xl m-4">Check your connection and try again.</h1>
     </div>
   );
 }
