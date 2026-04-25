38387810034cf475c0a96cc3f1e7bc63cc4ef145
g2kmedia <>
2026-04-25 13:13:18 +0200

    Fix: replaced window with pathname due to build issues

--- app/notes/@notesEditor/offline/page.tsx
+++ app/notes/@notesEditor/offline/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
 import NoteEditorPage from "@/app/components/NoteEditorPage";
+import { usePathname } from "next/navigation";
 
 export default function OfflineNote() {
-    const id = window.location.pathname.split("/").at(-1);
+    const pathname = usePathname();
+    
+    const id = pathname.split("/").at(-1);
 
     if (!id) return null;
 
